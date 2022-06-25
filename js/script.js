c373c717affe8633abadd46c592a923c22c8f4ac
TONNYCHWEYA <chweyatonny@gmail.com>
2022-06-25 08:15:02 +0300

    package update

--- js/script.js
+++ js/script.js
@@ -8,7 +8,11 @@ const height_feet =document.getElementById('height_feet')
 const height_inches = document.getElementById('height_inches')
 const position = document.getElementById('Position')
 const teamName = document.getElementById('teamName')
-
+const review = document.getElementById("review")
+const editReview = document.getElementById("editReview")
+const submitEdit = document.getElementById("submitEdit")
+const form = document.getElementById("form")
+//
 submitPlayer.addEventListener('click', (e) => {
   e.preventDefault()
   const playersName =player.value 
@@ -21,6 +25,15 @@ submitPlayer.addEventListener('click', (e) => {
     const li = document.createElement("li")
     li.textContent = element.first_name + " " + element.last_name
     ul.append(li)
+
+    // When you hover over the name of the players the color of the elements changes.
+    li.addEventListener("mouseover", (e) => {
+      li.style.color = 'red'
+    })
+
+    li.addEventListener("mouseout", (e) => {
+      li.style.color = 'black'
+    })
 
     const playerId = element.id;
     
@@ -35,6 +48,7 @@ submitPlayer.addEventListener('click', (e) => {
         position.textContent = data.team.division
         teamName.textContent = data.team.full_name
       })
+      e.target.style.backgroundColor = "green"
     })
     
   });
@@ -43,5 +57,11 @@ submitPlayer.addEventListener('click', (e) => {
   });
 })
 
+submitEdit.textContent = 'REVIEW'
+form.addEventListener("submit",(e) =>{
+  e.preventDefault()
+const reviewValue = editReview.value
+review.textContent = reviewValue
+})
 
 
